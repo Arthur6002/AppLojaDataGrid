663349bdfcd96a0cb8415c88ff87116f91989d89
Arthur6002 <arthurcesardefreitas@gmail.com>
2023-09-20 11:02:41 -0300

    Pronto

--- AppLojaDataGrid/Form1.Designer.cs
+++ AppLojaDataGrid/Form1.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtDescricao = new System.Windows.Forms.MaskedTextBox();
             this.txtQuantidade = new System.Windows.Forms.MaskedTextBox();
             this.txtValorUnitario = new System.Windows.Forms.MaskedTextBox();
-            this.txtSelecionados = new System.Windows.Forms.MaskedTextBox();
             this.btnInserir = new System.Windows.Forms.Button();
             this.btnRemover = new System.Windows.Forms.Button();
             this.btnAlterar = new System.Windows.Forms.Button();
@@ -50,42 +50,36 @@
             this.label6 = new System.Windows.Forms.Label();
             this.lblValorTotal = new System.Windows.Forms.Label();
             this.lblVenda = new System.Windows.Forms.Label();
+            this.lblQuantidadeItens = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
             this.SuspendLayout();
             // 
             // txtDescricao
             // 
-            this.txtDescricao.Location = new System.Drawing.Point(90, 56);
+            this.txtDescricao.Location = new System.Drawing.Point(170, 64);
             this.txtDescricao.Name = "txtDescricao";
             this.txtDescricao.Size = new System.Drawing.Size(441, 20);
             this.txtDescricao.TabIndex = 1;
             // 
             // txtQuantidade
             // 
-            this.txtQuantidade.Location = new System.Drawing.Point(90, 104);
+            this.txtQuantidade.Location = new System.Drawing.Point(170, 113);
             this.txtQuantidade.Name = "txtQuantidade";
             this.txtQuantidade.Size = new System.Drawing.Size(441, 20);
             this.txtQuantidade.TabIndex = 2;
             // 
             // txtValorUnitario
             // 
-            this.txtValorUnitario.Location = new System.Drawing.Point(90, 154);
+            this.txtValorUnitario.Location = new System.Drawing.Point(170, 166);
             this.txtValorUnitario.Name = "txtValorUnitario";
             this.txtValorUnitario.Size = new System.Drawing.Size(100, 20);
             this.txtValorUnitario.TabIndex = 3;
             // 
-            // txtSelecionados
-            // 
-            this.txtSelecionados.Location = new System.Drawing.Point(201, 416);
-            this.txtSelecionados.Name = "txtSelecionados";
-            this.txtSelecionados.Size = new System.Drawing.Size(265, 20);
-            this.txtSelecionados.TabIndex = 4;
-            // 
             // btnInserir
             // 
-            this.btnInserir.Location = new System.Drawing.Point(336, 151);
+            this.btnInserir.Location = new System.Drawing.Point(547, 210);
             this.btnInserir.Name = "btnInserir";
-            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.Size = new System.Drawing.Size(85, 66);
             this.btnInserir.TabIndex = 5;
             this.btnInserir.Text = "Inserir";
             this.btnInserir.UseVisualStyleBackColor = true;
@@ -93,9 +87,9 @@
             // 
             // btnRemover
             // 
-            this.btnRemover.Location = new System.Drawing.Point(456, 151);
+            this.btnRemover.Location = new System.Drawing.Point(547, 282);
             this.btnRemover.Name = "btnRemover";
-            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.Size = new System.Drawing.Size(85, 66);
             this.btnRemover.TabIndex = 6;
             this.btnRemover.Text = "Remover";
             this.btnRemover.UseVisualStyleBackColor = true;
@@ -103,9 +97,9 @@
             // 
             // btnAlterar
             // 
-            this.btnAlterar.Location = new System.Drawing.Point(472, 400);
+            this.btnAlterar.Location = new System.Drawing.Point(447, 392);
             this.btnAlterar.Name = "btnAlterar";
-            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.Size = new System.Drawing.Size(85, 66);
             this.btnAlterar.TabIndex = 7;
             this.btnAlterar.Text = "Alterar";
             this.btnAlterar.UseVisualStyleBackColor = true;
@@ -113,9 +107,9 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(472, 429);
+            this.btnCancelar.Location = new System.Drawing.Point(447, 464);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.Size = new System.Drawing.Size(85, 66);
             this.btnCancelar.TabIndex = 8;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -123,9 +117,9 @@
             // 
             // btnGravar
             // 
-            this.btnGravar.Location = new System.Drawing.Point(472, 458);
+            this.btnGravar.Location = new System.Drawing.Point(547, 392);
             this.btnGravar.Name = "btnGravar";
-            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.Size = new System.Drawing.Size(85, 66);
             this.btnGravar.TabIndex = 9;
             this.btnGravar.Text = "Gravar";
             this.btnGravar.UseVisualStyleBackColor = true;
@@ -133,12 +127,13 @@
             // 
             // btnFechar
             // 
-            this.btnFechar.Location = new System.Drawing.Point(472, 487);
+            this.btnFechar.Location = new System.Drawing.Point(547, 464);
             this.btnFechar.Name = "btnFechar";
-            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.Size = new System.Drawing.Size(85, 66);
             this.btnFechar.TabIndex = 10;
             this.btnFechar.Text = "Fechar";
             this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
             // dgvProdutos
             // 
@@ -150,7 +145,7 @@
             this.Descricao,
             this.dgvQuantidade,
             this.dgvValorunitario});
-            this.dgvProdutos.Location = new System.Drawing.Point(36, 201);
+            this.dgvProdutos.Location = new System.Drawing.Point(21, 193);
             this.dgvProdutos.Name = "dgvProdutos";
             this.dgvProdutos.ReadOnly = true;
             this.dgvProdutos.Size = new System.Drawing.Size(511, 183);
@@ -177,80 +172,99 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 15);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.Size = new System.Drawing.Size(86, 25);
             this.label1.TabIndex = 12;
             this.label1.Text = "Venda:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(26, 59);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(117, 25);
             this.label2.TabIndex = 13;
             this.label2.Text = "Descricao";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(26, 107);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.Size = new System.Drawing.Size(133, 25);
             this.label3.TabIndex = 14;
             this.label3.Text = "Quantidade";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(16, 161);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.Size = new System.Drawing.Size(153, 25);
             this.label4.TabIndex = 15;
             this.label4.Text = "Valor unitario";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 419);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 392);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(167, 13);
+            this.label5.Size = new System.Drawing.Size(370, 25);
             this.label5.TabIndex = 16;
             this.label5.Text = "Quantidade de itens selecionados";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(185, 465);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 466);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.Size = new System.Drawing.Size(60, 25);
             this.label6.TabIndex = 17;
             this.label6.Text = "Total";
             // 
             // lblValorTotal
             // 
             this.lblValorTotal.AutoSize = true;
-            this.lblValorTotal.Location = new System.Drawing.Point(264, 465);
+            this.lblValorTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorTotal.Location = new System.Drawing.Point(100, 466);
             this.lblValorTotal.Name = "lblValorTotal";
-            this.lblValorTotal.Size = new System.Drawing.Size(42, 13);
+            this.lblValorTotal.Size = new System.Drawing.Size(87, 25);
             this.lblValorTotal.TabIndex = 18;
             this.lblValorTotal.Text = "R$0,00";
             // 
             // lblVenda
             // 
             this.lblVenda.AutoSize = true;
-            this.lblVenda.Location = new System.Drawing.Point(87, 15);
+            this.lblVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVenda.Location = new System.Drawing.Point(114, 9);
             this.lblVenda.Name = "lblVenda";
-            this.lblVenda.Size = new System.Drawing.Size(13, 13);
+            this.lblVenda.Size = new System.Drawing.Size(25, 25);
             this.lblVenda.TabIndex = 19;
             this.lblVenda.Text = "1";
+            // 
+            // lblQuantidadeItens
+            // 
+            this.lblQuantidadeItens.AutoSize = true;
+            this.lblQuantidadeItens.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantidadeItens.Location = new System.Drawing.Point(21, 417);
+            this.lblQuantidadeItens.Name = "lblQuantidadeItens";
+            this.lblQuantidadeItens.Size = new System.Drawing.Size(25, 25);
+            this.lblQuantidadeItens.TabIndex = 20;
+            this.lblQuantidadeItens.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 544);
+            this.ClientSize = new System.Drawing.Size(648, 544);
+            this.Controls.Add(this.lblQuantidadeItens);
             this.Controls.Add(this.lblVenda);
             this.Controls.Add(this.lblValorTotal);
             this.Controls.Add(this.label6);
@@ -266,13 +280,13 @@
             this.Controls.Add(this.btnAlterar);
             this.Controls.Add(this.btnRemover);
             this.Controls.Add(this.btnInserir);
-            this.Controls.Add(this.txtSelecionados);
             this.Controls.Add(this.txtValorUnitario);
             this.Controls.Add(this.txtQuantidade);
             this.Controls.Add(this.txtDescricao);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Super Mercado";
             ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -283,7 +297,6 @@
         private System.Windows.Forms.MaskedTextBox txtDescricao;
         private System.Windows.Forms.MaskedTextBox txtQuantidade;
         private System.Windows.Forms.MaskedTextBox txtValorUnitario;
-        private System.Windows.Forms.MaskedTextBox txtSelecionados;
         private System.Windows.Forms.Button btnInserir;
         private System.Windows.Forms.Button btnRemover;
         private System.Windows.Forms.Button btnAlterar;
@@ -302,6 +315,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Descricao;
         private System.Windows.Forms.DataGridViewTextBoxColumn dgvQuantidade;
         private System.Windows.Forms.DataGridViewTextBoxColumn dgvValorunitario;
+        private System.Windows.Forms.Label lblQuantidadeItens;
     }
 }
 
--- AppLojaDataGrid/Form1.cs
+++ AppLojaDataGrid/Form1.cs
@@ -20,7 +20,7 @@ namespace AppLojaDataGrid
 
         private void btnInserir_Click(object sender, EventArgs e)
         {
-            double Quantidade = double.Parse(txtQuantidade.Text), ValorUnitario = double.Parse(txtQuantidade.Text);
+            double Quantidade = double.Parse(txtQuantidade.Text), ValorUnitario = double.Parse(txtValorUnitario.Text);
 
             dgvProdutos.Rows.Add(txtDescricao.Text, txtQuantidade.Text, txtValorUnitario.Text);
 
@@ -30,7 +30,9 @@ namespace AppLojaDataGrid
 
             ValorTotal += (Quantidade * ValorUnitario);
 
-            lblValorTotal.Text = ($"R${ValorTotal.ToString()}");
+            lblValorTotal.Text = (ValorTotal.ToString("C"));
+
+            lblQuantidadeItens.Text = (double.Parse(lblQuantidadeItens.Text) + 1).ToString();
         }
 
 
@@ -44,31 +46,44 @@ namespace AppLojaDataGrid
                 dgvProdutos.Rows.RemoveAt(dgvProdutos.CurrentRow.Index);
 
                 ValorTotal -= (Quantidade * ValorUnitario);
-                lblValorTotal.Text = ($"R${ValorTotal.ToString()}");
+                lblValorTotal.Text = (ValorTotal.ToString());
             }
         }
 
         private void btnAlterar_Click(object sender, EventArgs e)
         {
+            double Quantidade = double.Parse(dgvProdutos.CurrentRow.Cells[1].Value.ToString());
+            double ValorUnitario = double.Parse(dgvProdutos.CurrentRow.Cells[2].Value.ToString());
+
             dgvProdutos.CurrentRow.Cells[0].Value = txtDescricao.Text;
             dgvProdutos.CurrentRow.Cells[1].Value = txtQuantidade.Text;
             dgvProdutos.CurrentRow.Cells[2].Value = txtValorUnitario.Text;
+
+            ValorTotal -= Quantidade * ValorUnitario;
+            ValorTotal += double.Parse(txtValorUnitario.Text) * double.Parse(txtQuantidade.Text);
+
+            lblValorTotal.Text = ValorTotal.ToString("C");
+
+            txtDescricao.Clear();
+            txtQuantidade.Clear();
+            txtValorUnitario.Clear();
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             txtDescricao.Clear();
             txtQuantidade.Clear();
-            txtSelecionados.Clear();
+            lblQuantidadeItens.Text = "0";
             txtValorUnitario.Clear();
             dgvProdutos.Rows.Clear();
+            lblValorTotal.Text = "R$0";
         }
 
         private void btnGravar_Click(object sender, EventArgs e)
         {
             txtDescricao.Clear();
             txtQuantidade.Clear();
-            txtSelecionados.Clear();
+            lblQuantidadeItens.Text = "0";
             txtValorUnitario.Clear();
             dgvProdutos.Rows.Clear();
 
@@ -77,6 +92,11 @@ namespace AppLojaDataGrid
             lblVenda.Text = Venda.ToString();
 
             lblValorTotal.Text = "R$0";
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
